f47adeccdff331eec5cc3b7afcb727dc97a16905
IB_Student <student@InnovateBhams-MacBook-Pro-2.local>
2020-02-04 14:02:02 -0600

    completed lab files

--- script.js
+++ script.js
@@ -13,23 +13,39 @@ function accuseNumber() {
         h3.append(h3Text);
         textHolder.append(h3);
         console.log(h3Text);
+
+        (h3).click(accuseClicker(i));
     };
 
-};
+    // var j = friends.length % locations.length % weapons.length;
+    
+    function accuseClicker(i) {
+        return function () {
+            var d = i % friends.length;
+            var j = i % weapons.length;
+            var p = i % locations.length;
 
-$('h3').on('click', function (event) {
-    // alert('hey!');
-    // var h3 = $('<h3></h3>');
-    // var h3Text = 'Accusation ' + [i];
-    // h3.append(h3Text);
-    // textHolder.append(h3);
-    // console.log(h3Text);
-    i=0;
-    j=0;
-    for (var j = 0; i < friends.length;) {
-    alert ('Accusation ' + [i] + ': I accuse ' + friends[j] + ', with the ' + weapons[i] + ' in the ' + locations[i] + '!');
-    j++;
+            alert(`Accusation ${i}: I accuse ${friends[d]}, with the ${weapons[j]} in the ${locations[p]}!`);
+
+        };
+
     };
-    // alert(alertText);
-});
+
+// function accuseClicker() {
+
+//     return $('h3').on('click', function (event) {
+//         i = 1
+//         // var h3Text = 'Accusation ' + [i];
+//         // alert(h3Text + ': I accuse ' + friends[j] + ', with the ' + weapons[d] + ' in the ' + locations[p] + '!');
+//         alert(`Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${weapons[i % weapons.length]} in the ${locations[i % locations.length]}!`);
+
+//         // i++;
+//         // j++;
+//         // d++;
+//         // p++;
+
+//     });
+ };
+
+// CREATED A CLOSURE BY PUTTING THIS FUNCTION WITHIN THE FOR LOOP AS A RETURN. I ALSO RESTRUCTURED THE VARIABLES AND THE ALERT A BIT.
 
